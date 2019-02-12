74fb940b5721fdc922879514155346251bd96489
Guillermo García López <memogl25@gmail.com>
2019-02-12 17:57:25 -0600

    refactor CSVForm.vue to upload several files

--- src/components/CSVForm.vue
+++ src/components/CSVForm.vue
@@ -2,14 +2,13 @@
   <div id="csv-form">
     <p>Sube un archivo CSV con <br> más números consecutivos</p>
     <form @submit.prevent="onUpload">
-      <input type="file" name="csvfile" />
+      <input type="file" name="csvfile" accept=".csv" multiple />
       <input type="submit" value="Enviar archivo" />
     </form>
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
   import { mapActions } from 'vuex';
 
   export default {
@@ -17,20 +16,23 @@
     methods: {
       ...mapActions(['uploadCSVFile', 'populateTickets']),
       onUpload: function (event) {
-        let CSVFile = event.target.elements[0].files[0];
-
-        if (typeof CSVFile === 'undefined') {
-          alert('Debe seleccionar un archivo tipo .csv');
+        const CSVFiles = event.target.elements[0].files;
+        if (!CSVFiles.length) {
+          alert('Debe seleccionar al menos un archivo tipo .csv');
           return;
         }
 
-        if (!CSVFile.name.toLowerCase().endsWith('.csv')) {
-          alert('El archivo a enviar debe de ser tipo csv (comma-separated values)');
-          return;
+        for (let CSVFile of Object.values(CSVFiles)) {
+          if (!CSVFile.name.toLowerCase().endsWith('.csv') || CSVFile.type !== 'text/csv') {
+            alert(`El archivo ${CSVFile.name} debe de ser de tipo csv (comma-separated values).`);
+            return;
+          }
         }
 
-        let form = new FormData();
-        form.append('csvfile', CSVFile, CSVFile.name);
+        const form = new FormData();
+        for (let CSVFile of Object.values(CSVFiles)) {
+          form.append('csvfile', CSVFile, CSVFile.name);
+        }
 
         this.uploadCSVFile(form).then((result) => {
           alert(result.message);
